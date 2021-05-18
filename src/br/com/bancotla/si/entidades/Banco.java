c9a3e49191aefe1d57513aac84dc4acb7c8b6c3b
Thiago Almeida <t.leandroalmeida@gmail.com>
2021-05-18 09:46:57 -0300

    Criando a classe Banco incompleta

---
+++ src/br/com/bancotla/si/entidades/Banco.java
@@ -0,0 +1,14 @@
+package br.com.bancotla.si.entidades;
+
+public class Banco {
+
+	private String razaoSocial;
+	private String nomeFantasia;
+	private long cnpj;
+	private String endereco;
+	
+	// completar variaveis (lista de agencias)
+	// construtores + getters and setters
+	// toString
+	// metodos específicos: listar todas as contas / listas capital total investido / numero total de clientes..
+}
